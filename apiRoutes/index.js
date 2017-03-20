16212af3aa499d6bf191905a760410a261bfe9fe
Jeff Hager <jeffrey.hager.dev@gmail.com>
2017-03-20 14:07:25 -0500

    Adding additional API end points

--- apiRoutes/index.js
+++ apiRoutes/index.js
@@ -5,7 +5,7 @@ module.exports = function(socketHandler){
 
     /**
      * @swagger
-     * api/hello:
+     * api/checkin:
      *   get:
      *     produces:
      *       - application/json
@@ -13,9 +13,54 @@ module.exports = function(socketHandler){
      *       200:
      *         description: a greeting
      */
-    router.get('/api/hello', function(req, res){
-        socketHandler.helloWorld();
-        res.status(200).json({"message": "hello world"});
+    router.get('/api/checkin', function(req, res){
+        socketHandler.checkin();
+        res.status(200).json({"message": "checked in"});
+    });
+
+    /**
+     * @swagger
+     * api/standing:
+     *   get:
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: a greeting
+     */
+    router.get('/api/standing', function(req, res){
+        socketHandler.stand();
+        res.status(200).json({"message": "success"});
+    });
+
+    /**
+     * @swagger
+     * api/color:
+     *   get:
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: a greeting
+     */
+    router.get('/api/color', function(req, res){
+        socketHandler.setColor();
+        res.status(200).json({"message": "success"});
+    });
+
+    /**
+     * @swagger
+     * api/reaction:
+     *   get:
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: a greeting
+     */
+    router.get('/api/reaction', function(req, res){
+        socketHandler.reaction();
+        res.status(200).json({"message": "success"});
     });
 
 
