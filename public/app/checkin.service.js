9d2809403e352eaf04c7718b694be7e7da5649dc
Jeff Hager <jeffrey.hager.dev@gmail.com>
2017-03-21 12:10:59 -0500

    Missing files

---
+++ public/app/checkin.service.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var Observable_1 = require('rxjs/Observable');
+var io = require('socket.io-client');
+var core_1 = require('@angular/core');
+var CheckinService = (function () {
+    function CheckinService() {
+        this.url = 'http://localhost:3005';
+    }
+    CheckinService.prototype.getUsers = function () {
+        var _this = this;
+        var observable = new Observable_1.Observable(function (observer) {
+            _this.socket = io(_this.url);
+            _this.socket.on('checkin', function (data) {
+                observer.next(data);
+            });
+            return function () {
+                _this.socket.disconnect();
+            };
+        });
+        return observable;
+    };
+    CheckinService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [])
+    ], CheckinService);
+    return CheckinService;
+}());
+exports.CheckinService = CheckinService;
+//# sourceMappingURL=checkin.service.js.map
