fea41bfeb30d0a8fbd98b64a57b0a394e6d19706
Lauren Hilton <lauren@stl22maclmh.daugherty.com>
2017-03-23 14:13:08 -0500

    add user names to reaction times

--- public/app/reaction/reaction.component.js
+++ public/app/reaction/reaction.component.js
@@ -10,11 +10,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var reaction_service_1 = require('./reaction.service');
+var checkin_service_1 = require('../checkin.service');
 var moment = require('moment');
 var ReactionComponent = (function () {
-    function ReactionComponent(reactionService) {
+    function ReactionComponent(reactionService, checkinService) {
         this.reactionService = reactionService;
+        this.checkinService = checkinService;
+        this.users = [];
         this.reactions = [];
+        this.disqualified = [];
         this.countdown = 3;
         this.isCountdown = false;
     }
@@ -46,10 +50,43 @@ var ReactionComponent = (function () {
             id: reaction.number,
             time: s
         };
+        for (var _i = 0, _a = this.users; _i < _a.length; _i++) {
+            var user = _a[_i];
+            if (user.number == (newReaction.id)) {
+                newReaction.id = user.name;
+            }
+        }
         var existing = this.reactions.filter(function (reaction) { return reaction.id == newReaction.id; });
         if (existing.length == 0 && this.startTime) {
             this.reactions.push(newReaction);
         }
+        else if (existing.length == 0 && !this.startTime) {
+            this.disqualified.push(newReaction);
+        }
+    };
+    ReactionComponent.prototype.checkInUsers = function () {
+        var _this = this;
+        this.checkInConnection = this.checkinService.getUsers().subscribe(function (data) {
+            if (data instanceof Array && data.length > 0) {
+                for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+                    var info = data_1[_i];
+                    for (var _a = 0, _b = _this.users; _a < _b.length; _a++) {
+                        var user = _b[_a];
+                        if (user.number == (info.number || -1)) {
+                            user.name = info.name;
+                        }
+                    }
+                }
+            }
+            else {
+                for (var _c = 0, _d = _this.users; _c < _d.length; _c++) {
+                    var user = _d[_c];
+                    if (user.number == (data.number || -1)) {
+                        user.name = data.name;
+                    }
+                }
+            }
+        });
     };
     ReactionComponent.prototype.ngOnInit = function () {
         var _this = this;
@@ -57,18 +94,20 @@ var ReactionComponent = (function () {
             _this.createNewReaction(reaction);
             console.log(reaction);
         });
+        this.checkInUsers();
     };
     ReactionComponent.prototype.ngOnDestroy = function () {
         this.connection.unsubscribe();
+        this.checkInConnection.unsubscribe();
     };
     ReactionComponent = __decorate([
         core_1.Component({
             selector: 'reaction-component',
             templateUrl: './app/reaction/reaction.component.html',
             styleUrls: ['./app/reaction/reaction.component.css'],
-            providers: [reaction_service_1.ReactionService]
+            providers: [reaction_service_1.ReactionService, checkin_service_1.CheckinService]
         }), 
-        __metadata('design:paramtypes', [reaction_service_1.ReactionService])
+        __metadata('design:paramtypes', [reaction_service_1.ReactionService, checkin_service_1.CheckinService])
     ], ReactionComponent);
     return ReactionComponent;
 }());
--- public/app/reaction/reaction.component.ts
+++ public/app/reaction/reaction.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import { ReactionService } from './reaction.service';
+import { CheckinService }       from '../checkin.service';
 import * as moment from 'moment';
 
 @Component({
     selector: 'reaction-component',
     templateUrl: './app/reaction/reaction.component.html',
     styleUrls: ['./app/reaction/reaction.component.css'],
-    providers: [ReactionService]
+    providers: [ReactionService, CheckinService]
 })
 
 export class ReactionComponent implements OnInit, OnDestroy {
+    users: any[] = [];
     reactions: any[] = [];
+    disqualified: any[] = [];
     connection: any;
     message: any;
     countdown: any = 3;
     isCountdown: boolean = false;
     startTime: Date;
+    checkInConnection: any;
 
-    constructor(private reactionService:ReactionService) {}
+    constructor(private reactionService:ReactionService,
+                private checkinService: CheckinService) {}
 
     startCountdown() {
         this.isCountdown = true;
@@ -48,20 +53,50 @@ export class ReactionComponent implements OnInit, OnDestroy {
             id: reaction.number,
             time: s
         };
+        for(let user of this.users) {
+            if(user.number == (newReaction.id)){
+                newReaction.id = user.name;
+            }
+        }
         let existing = this.reactions.filter(reaction => reaction.id == newReaction.id);
         if (existing.length == 0 && this.startTime) {
             this.reactions.push(newReaction);
+        } else if (existing.length == 0 && !this.startTime) {
+            this.disqualified.push(newReaction);
         }
+    }
+
+    checkInUsers(){
+        this.checkInConnection = this.checkinService.getUsers().subscribe((data: any) => {
+            if(data instanceof Array && data.length > 0){
+                for(var info of data){
+                    for(var user of this.users) {
+                        if(user.number == (info.number||-1)){
+                            user.name = info.name;
+                        }
+                    }
+                }
+            }
+            else{
+                for(var user of this.users) {
+                    if(user.number == (data.number||-1)){
+                        user.name = data.name;
+                    }
+                }
+            }
+        });
     }
 
     ngOnInit() {
         this.connection = this.reactionService.getReactions().subscribe(reaction => {
             this.createNewReaction(reaction);
             console.log(reaction);
-        })
+        });
+        this.checkInUsers();
     }
 
     ngOnDestroy() {
         this.connection.unsubscribe();
+        this.checkInConnection.unsubscribe();
     }
 }
