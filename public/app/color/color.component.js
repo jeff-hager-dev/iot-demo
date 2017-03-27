825bac2b6b1c69bc2fb42e8f424c34459af9e230
Sallee <MDS1212@daugherty.com>
2017-03-27 15:53:05 -0500

    start animate colors

--- public/app/color/color.component.js
+++ public/app/color/color.component.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var color_service_1 = require('./color.service');
 var angular2_color_picker_1 = require('angular2-color-picker');
+var TimerObservable_1 = require('rxjs/observable/TimerObservable');
 var Color = (function () {
     function Color() {
     }
@@ -23,6 +24,8 @@ var ColorComponent = (function () {
         this.colors = [];
         this.color = '#fff';
         this.count = 20;
+        this.animateCount = 0;
+        this.animateColors = ['#ff0000', '#ff7f00', '#FFFF00', '#00ff00', '#00ffff', '#0000ff', '#8B00ff', '#8B00ff', '#0000ff', '#00ffff', '#00ff00', '#FFFF00', '#ff7f00', '#ff0000'];
     }
     ColorComponent.prototype.setColor = function (color, user) {
         this.colorService.setColor(user, color.slice(1));
@@ -32,6 +35,30 @@ var ColorComponent = (function () {
         this.colors.forEach(function (userColor) {
             userColor.color = color;
             _this.colorService.setColor(userColor.number, userColor.color.slice(1));
+        });
+    };
+    ColorComponent.prototype.animate = function () {
+        var _this = this;
+        console.log('click worked!');
+        this.animateCount = 0;
+        var timer = TimerObservable_1.TimerObservable.create(200, 300);
+        this.subscription = timer.subscribe(function (t) {
+            console.log('tick: ' + _this.animateCount);
+            var index = _this.colors.findIndex(function (c) { return c.number == 1; });
+            if (index > -1) {
+                _this.colors[index].color = _this.animateColors[_this.animateCount];
+            }
+            else {
+                _this.colors.push({
+                    number: 1,
+                    color: _this.animateColors[_this.animateCount]
+                });
+            }
+            _this.colorService.setColor(1, _this.animateColors[_this.animateCount]);
+            _this.animateCount++;
+            if (_this.animateCount > 14) {
+                _this.subscription.unsubscribe();
+            }
         });
     };
     ColorComponent.prototype.setupColors = function () {
--- public/app/color/color.component.ts
+++ public/app/color/color.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ColorService } from './color.service';
 import { ColorPickerService, Rgba } from 'angular2-color-picker';
+import { Subscription } from 'rxjs';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
 export class Color {
     number: number;
@@ -17,6 +19,9 @@ export class ColorComponent implements OnInit, OnDestroy {
     colors: Color[] = [];
     color: string = '#fff';
     count: number = 20;
+    animateCount: number = 0;
+    animateColors: string[] = ['#ff0000', '#ff7f00', '#FFFF00', '#00ff00', '#00ffff', '#0000ff', '#8B00ff', '#8B00ff', '#0000ff', '#00ffff', '#00ff00', '#FFFF00', '#ff7f00', '#ff0000'];
+    subscription: Subscription;
 
     constructor(private colorService: ColorService) {
     }
@@ -29,6 +34,27 @@ export class ColorComponent implements OnInit, OnDestroy {
         this.colors.forEach((userColor) => {
            userColor.color = color;
            this.colorService.setColor(userColor.number, userColor.color.slice(1));
+        });
+    }
+
+    animate() {
+        console.log('click worked!');
+        this.animateCount = 0;
+        let timer = TimerObservable.create(200, 300);
+        this.subscription = timer.subscribe(t => {
+            console.log('tick: ' + this.animateCount);
+            let index = this.colors.findIndex(c => c.number == 1)
+            if (index > -1) { 
+                this.colors[index].color = this.animateColors[this.animateCount];
+            } else {
+                this.colors.push({
+                    number: 1,
+                    color: this.animateColors[this.animateCount]
+                });
+            }
+            this.colorService.setColor(1, this.animateColors[this.animateCount]);
+            this.animateCount++;
+            if (this.animateCount > 14) { this.subscription.unsubscribe(); }
         });
     }
 
